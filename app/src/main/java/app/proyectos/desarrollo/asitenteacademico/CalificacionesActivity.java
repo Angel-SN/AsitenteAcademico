0bd716ce49fabcfdaad2c3de7694b0b9fdee53ec
Hector Stark <Hector_Stark@github.com>
2017-11-19 20:49:58 -0600

    clases
    .java

---
+++ app/src/main/java/app/proyectos/desarrollo/asitenteacademico/CalificacionesActivity.java
@@ -0,0 +1,27 @@
+package app.proyectos.desarrollo.asitenteacademico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class CalificacionesActivity extends AppCompatActivity {
+
+    private EditText Cal;
+    private EditText porc;
+    private Button btnCal;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calificacion);
+
+
+    }
+
+    private void bindUI(){
+        Cal = (EditText) findViewById(R.id.Cal);
+        porc = (EditText)findViewById(R.id.porc);
+        btnCal = (Button) findViewById(R.id.btnCal);
+    }
+}
---
+++ app/src/main/java/app/proyectos/desarrollo/asitenteacademico/EventoActivity.java
@@ -0,0 +1,29 @@
+package app.proyectos.desarrollo.asitenteacademico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class EventoActivity extends AppCompatActivity {
+
+    private EditText Even;
+    private EditText mate;
+    private EditText Descr;
+    private Button btnEven;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_evento);
+
+
+    }
+
+    private void bindUI(){
+        Even = (EditText) findViewById(R.id.Even);
+        mate = (EditText)findViewById(R.id.mate);
+        Descr = (EditText)findViewById(R.id.Descr);
+        btnEven = (Button) findViewById(R.id.btnEven);
+    }
+}
